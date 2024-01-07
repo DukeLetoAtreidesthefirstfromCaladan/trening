9402d5f118bfe9f12344e4bdbb12889b661e8bc7
DukeLetoAtreidesthefirstfromCaladan <114316213+DukeLetoAtreidesthefirstfromCaladan@users.noreply.github.com>
2024-01-07 16:34:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<php> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    
+</head>
+<body>
+    
+    
+    <div class="stage2">
+        <?php include('haupt/haupt.html');?>
+    </div>
+    
+    <div class="stage3">
+        <?php include('footer/footer.html');?>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
+
+
+</php>
